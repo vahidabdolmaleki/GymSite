62c30ef8636428c919697983389fbd99b3154bc6
vahid abdolmaleki <v.abdolmaleki67@gmail.com>
2025-10-14 07:07:24 +0330

    Create WorkoutMedia entity and add to Dbset

--- DAL/Context/GymDbContext.cs
+++ DAL/Context/GymDbContext.cs
@@ -55,6 +55,7 @@ namespace DAL.Context
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<UserMembership> UserMemberships { get; set; }
         public DbSet<WorkoutHistory> WorkoutHistories { get; set; }
+        public DbSet<WorkoutMedia> WorkoutMedias { get; set; }
         
         public DbSet<RolePermission> RolePermissions{ get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ Entities/WorkoutMedia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class WorkoutMedia : BaseEntity
+    {
+        public int WorkoutId { get; set; }              // تمرینی که فایل بهش تعلق داره
+        public Workout Workout { get; set; } = null!;
+
+        public string FileUrl { get; set; } = null!;    // مسیر فایل (روی سرور یا CDN)
+        public string? ThumbnailUrl { get; set; }       // تصویر بندانگشتی برای ویدیوها
+        public MediaType Type { get; set; }             // نوع فایل (عکس یا ویدیو)
+        public string? Description { get; set; }        // توضیح یا عنوان فایل (مثلاً "حرکت پرس سینه با دمبل")
+
+        public enum MediaType
+        {
+            Image = 0,
+            Video = 1
+        }
+    }
+}
