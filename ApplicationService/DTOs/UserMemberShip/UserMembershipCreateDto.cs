f763325a849ea69d5e44e58f2916a2fbbd813f07
vahid abdolmaleki <v.abdolmaleki67@gmail.com>
2025-11-25 14:31:00 +0330

    Create Dtos for Creat and get Membership in ApplicationService

---
+++ ApplicationService/DTOs/UserMemberShip/UserMembershipCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationService.DTOs.UserMemberShip
+{
+    public class UserMembershipCreateDto
+    {
+        public int PersonId { get; set; }
+        public int MembershipId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public int DurationDays { get; set; }
+    }
+
+}
---
+++ ApplicationService/DTOs/UserMemberShip/UserMembershipDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ApplicationService.DTOs.UserMemberShip
+{
+    public class UserMembershipDto
+    {
+        public int Id { get; set; }
+        public string PersonFullName { get; set; }
+        public string MembershipTitle { get; set; }
+        public string CreatedBy { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+}
