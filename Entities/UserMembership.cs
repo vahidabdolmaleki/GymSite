4f8c77dc00e4141ff2884a6ee2d8166439ce8b97
vahid abdolmaleki <v.abdolmaleki67@gmail.com>
2025-11-25 14:25:17 +0330

    change Usermembership entity and add migration

--- Entities/UserMembership.cs
+++ Entities/UserMembership.cs
@@ -3,13 +3,18 @@
     //برای نمایش وضعیت اشتراک
     public class UserMembership : BaseEntity
     {
-        public int UserId { get; set; }
-        public User User { get; set; } = null!;
+        public int PersonId { get; set; }
+        public Person Person { get; set; } = null!;
+
         public int MembershipId { get; set; }
         public Membership Membership { get; set; } = null!;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
+
 }
 
 
