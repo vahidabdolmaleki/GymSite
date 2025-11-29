2abbf146cbc52a9cd0b9f5626bb64c4f0bf2cecb
vahid abdolmaleki <v.abdolmaleki67@gmail.com>
2025-11-29 13:56:19 +0330

    Create Notification Dtos For use in Controller and applicationService

---
+++ ApplicationService/DTOs/Notification/NotificationCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+namespace ApplicationService.DTOs
+{
+    public class NotificationCreateDto
+    {
+        public int PersonId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public Notification.NotificationType Type { get; set; }
+        public string? MetaData { get; set; }
+    }
+
+}
---
+++ ApplicationService/DTOs/Notification/NotificationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ApplicationService.DTOs
+{
+    public class NotificationDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public bool IsRead { get; set; }
+        public DateTime SentAt { get; set; }
+        public string? MetaData { get; set; }
+    }
+
+}
