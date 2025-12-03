b39526fb85b774c5f7de7895dc9af1e81362fb0a
vahid abdolmaleki <v.abdolmaleki67@gmail.com>
2025-12-03 07:29:31 +0330

    For Test Commit and Push

--- GymSite.API/Controllers/TestController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-[ApiController]
-[Route("api/[controller]")]
-public class TestController : ControllerBase
-{
-    [HttpGet]
-    public IActionResult Get() => Ok("API Works!");
-}
